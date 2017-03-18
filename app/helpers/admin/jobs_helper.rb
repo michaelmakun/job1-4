d0678526fd33139bb2f60dad15f42c45472c7bad
makun <michaelmakun@aliyun.com>
2017-03-18 16:59:02 +0800

    publish and hide job

--- app/helpers/admin/jobs_helper.rb
+++ app/helpers/admin/jobs_helper.rb
@@ -2,9 +2,9 @@ module Admin::JobsHelper
 
   def render_job_status(job)
     if job.is_hidden
-      "(Hidden)"
+      content_tag(:span, "", :class => "fa fa-lock fa-lg")
     else
-      "(Public)"
+      content_tag(:span, "", :class => "fa fa-eye fa-lg")
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
   resources :jobs
 
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        get :publish
+        get :hide
+      end
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
